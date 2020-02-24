e23dd12b04b44490670c4c0a48810aa14364de96
Daichi Kimura <daichi703n@gmail.com>
2020-02-24 17:44:38 +0900

    Add settings for alert

--- commands/upgrade.sh
+++ commands/upgrade.sh
@@ -1,1 +1,2 @@
-helm upgrade --recreate-pods --cleanup-on-fail --install praeco --namespace elastic praeco-0.1.0.tgz
+helm upgrade --recreate-pods --install praeco --namespace elastic praeco-0.1.0.tgz \
+  -f vars.yml

45e2f792326f47d84c4cac22c9efdcc1bc7682be
Daichi Kimura <daichi703n@gmail.com>
2026-01-18 11:39:18 +0000

    Bump to 0.1.8

--- commands/upgrade.sh
+++ commands/upgrade.sh
@@ -1,2 +1,2 @@
-helm upgrade --recreate-pods --install praeco --namespace elastic praeco-0.1.7.tgz \
+helm upgrade --recreate-pods --install praeco --namespace elastic praeco-0.1.8.tgz \
   -f vars.yml
